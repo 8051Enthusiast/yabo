cb3b33fa773085208a43c6b2a6c4e0cf028edd21
8051enthusiast <8051enthusiast@protonmail.com>
2022-06-29 13:34:26 +0200

    move big thing out of small thing

--- src/order/expr.rs
+++ src/order/expr.rs
@@ -31,32 +31,31 @@ pub fn resolve_expr(db: &dyn Orders, expr_id: hir::ExprId) -> SResult<TypedResol
     let expr = db.parser_expr_at(expr_id)?;
     let parent_block = db.hir_parent_block(expr_id.0)?;
     expr.convert_niladic(&mut |x| {
-        Ok(OpWithData {
-            inner: match &x.inner {
-                hir::ParserAtom::Atom(Atom::Number(s)) => ResolvedAtom::Number(s.to_string()),
-                hir::ParserAtom::Atom(Atom::Char(s)) => ResolvedAtom::Char(s.to_string()),
-                hir::ParserAtom::Single => ResolvedAtom::Single,
-                hir::ParserAtom::Block(b) => ResolvedAtom::Block(*b),
-                hir::ParserAtom::Atom(Atom::Field(f)) => {
-                    let (id, kind) =
-                        hir::refs::resolve_var_ref(db, expr_id.0, *f)?.ok_or(SilencedError)?;
-                    match kind {
-                        hir::refs::VarType::ParserDef => {
-                            ResolvedAtom::ParserDef(hir::ParserDefId(id))
-                        }
-                        hir::refs::VarType::Value => {
-                            let is_captured = parent_block
-                                .map(|x| !x.0.is_ancestor_of(db, id))
-                                .unwrap_or(false);
-                            if is_captured {
-                                ResolvedAtom::Captured(id)
-                            } else {
-                                ResolvedAtom::Val(id)
-                            }
+        let inner = match &x.inner {
+            hir::ParserAtom::Atom(Atom::Number(s)) => ResolvedAtom::Number(s.to_string()),
+            hir::ParserAtom::Atom(Atom::Char(s)) => ResolvedAtom::Char(s.to_string()),
+            hir::ParserAtom::Single => ResolvedAtom::Single,
+            hir::ParserAtom::Block(b) => ResolvedAtom::Block(*b),
+            hir::ParserAtom::Atom(Atom::Field(f)) => {
+                let (id, kind) =
+                    hir::refs::resolve_var_ref(db, expr_id.0, *f)?.ok_or(SilencedError)?;
+                match kind {
+                    hir::refs::VarType::ParserDef => ResolvedAtom::ParserDef(hir::ParserDefId(id)),
+                    hir::refs::VarType::Value => {
+                        let is_captured = parent_block
+                            .map(|x| !x.0.is_ancestor_of(db, id))
+                            .unwrap_or(false);
+                        if is_captured {
+                            ResolvedAtom::Captured(id)
+                        } else {
+                            ResolvedAtom::Val(id)
                         }
                     }
                 }
-            },
+            }
+        };
+        Ok(OpWithData {
+            inner,
             data: x.data,
         })
     })
