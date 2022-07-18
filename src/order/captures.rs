e2f83b1fd2ab6cddb9df5af49caec6a2298b0374
8051enthusiast <8051enthusiast@protonmail.com>
2022-07-18 13:04:10 +0200

    fix captures not returning anything
    it was going from the top block, but children of blocks were excluded,
    so it didn't iterate over anything

--- src/order/captures.rs
+++ src/order/captures.rs
@@ -1,7 +1,8 @@
 use std::{collections::BTreeSet, sync::Arc};
 
 use crate::{
-    hir::{self, walk::ChildIter},
+    dbeprintln,
+    hir::{self, walk::ChildIter, HirIdWrapper},
     interner::DefId,
 };
 
@@ -9,21 +10,22 @@ use super::Orders;
 
 pub fn captures(db: &dyn Orders, id: hir::BlockId) -> Arc<BTreeSet<DefId>> {
     let mut ret = BTreeSet::new();
-    for i in ChildIter::new(id.0, db).without_kinds(hir::HirNodeKind::Block) {
-        match i {
-            hir::HirNode::Expr(expr) => {
-                ret.extend(
-                    hir::refs::expr_value_refs(db, &expr, expr.id.0)
-                        .filter(|target| !id.0.is_ancestor_of(db, *target)),
-                );
-            }
-            hir::HirNode::Block(block) => {
-                if block.id != id {
-                    ret.extend(db.captures(block.id).iter());
-                }
-            }
-            _ => continue,
+    let root_context = match id.lookup(db) {
+        Ok(x) => x,
+        Err(_) => return Default::default(),
+    }
+    .root_context;
+    for i in ChildIter::new(root_context.0, db).without_kinds(hir::HirNodeKind::Block) {
+        if let hir::HirNode::Expr(expr) = i {
+            ret.extend(
+                hir::refs::expr_value_refs(db, &expr, expr.id.0)
+                    .filter(|target| !id.0.is_ancestor_of(db, *target)),
+            );
         }
+    }
+    dbeprintln!(db, "{}:", &id.0);
+    for i in ret.iter() {
+        dbeprintln!(db, "\t{}", i);
     }
     Arc::new(ret)
 }
