86c18d741d7d309db052876d5b6509cd5ff3416e
8051enthusiast <8051enthusiast@protonmail.com>
2023-07-08 00:25:03 +0200

    don't use ugly tuple
    make a struct to make it more readable, no more .0
    also shuts up clippy's type complexity hint

--- crates/yaboc-cg-llvm/src/funs.rs
+++ crates/yaboc-cg-llvm/src/funs.rs
@@ -428,7 +428,7 @@ impl<'llvm, 'comp> CodeGenCtx<'llvm, 'comp> {
         }
 
         let return_layout = self.layouts.block_result()[&(from, layout.inner())]
-            .0
+            .val()
             .as_ref()
             .unwrap()
             .returned;
