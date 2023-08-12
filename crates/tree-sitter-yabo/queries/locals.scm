cf7030f160ef7663619dfccc1016e517743f39e4
8051enthusiast <8051enthusiast@protonmail.com>
2023-08-12 20:43:23 +0200

    add syntax highlighting to tree-sitter-yabo

---
+++ crates/tree-sitter-yabo/queries/locals.scm
@@ -0,0 +1,22 @@
+; scopes
+(parser_definition) @local.scope
+
+(parser_block) @local.scope
+
+
+; definitions
+(arg_definition
+    name: (identifier) @local.definition)
+
+(let_statement
+    name: (identifier) @local.definition)
+
+(parse_statement
+    name: (identifier) @local.definition)
+
+(import
+    name: (identifier) @local.definition)
+
+
+; references
+(identifier) @local.reference
