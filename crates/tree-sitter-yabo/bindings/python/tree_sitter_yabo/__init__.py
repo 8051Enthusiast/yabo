1fec2ae5a10cb28c110475afd5d778a0decafec1
8051enthusiast <8051enthusiast@protonmail.com>
2024-06-20 15:34:21 +0200

    regenerate tree-sitter parser

---
+++ crates/tree-sitter-yabo/bindings/python/tree_sitter_yabo/__init__.py
@@ -0,0 +1,5 @@
+"Yabo grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
---
+++ crates/tree-sitter-yabo/bindings/swift/TreeSitterYabo/yabo.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_YABO_H_
+#define TREE_SITTER_YABO_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_yabo(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_YABO_H_
