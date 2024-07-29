1ab42d7f2f988f303c162a009947aea6b66c0eb6
8051enthusiast <8051enthusiast@protonmail.com>
2024-07-30 00:09:32 +0200

    ybq: read programs line-by-line

--- tools/ybq/app/Main.hs
+++ tools/ybq/app/Main.hs
@@ -2,28 +2,30 @@ module Main (main) where
 
 import Control.Monad.Trans (liftIO)
 import Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
-import Lib (getParser, openLibrary, parse)
+import Lib (YaboVal, getParser, openLibrary, parse)
 import System.Environment (getArgs)
 import System.IO.MMap (mmapFileByteString)
 
+procLine :: (Int -> YaboVal) -> String -> IO ()
+procLine parser line = do
+  parseResult <- parse line "stdin"
+  case parseResult of
+    Left err -> print err
+    Right program -> print $ program $ parser 0
+
 main :: IO ()
 main =
-  runMaybeT
-    ( do
-        args <- liftIO getArgs
-        let (libName, parserName, program, inputFile) = case args of
-              [library, parser, prog, input] -> (library, parser, prog, input)
-              _ -> error "Usage: ybq <library> <parser> <program> <input>"
-        parseResult <- liftIO $ parse program "cmdline"
-        parsed <- case parseResult of
-          Left err -> do
-            liftIO $ print err
-            MaybeT $ return Nothing
-          Right p -> return p
-        file <- liftIO $ mmapFileByteString inputFile Nothing
-        lib <- MaybeT $ openLibrary libName file
-        parser <- MaybeT $ getParser lib parserName
-        let res = parser 0
-        liftIO $ print $ parsed res
-    )
-    >>= maybe (pure ()) return
+  runMaybeT prog >>= maybe (pure ()) return
+  where
+    prog = do
+      args <- liftIO getArgs
+      (libName, parserName, inputFile) <- case args of
+        [library, parser, input] -> return (library, parser, input)
+        _ -> do
+          liftIO $ print "Usage: ybq <library> <parser> <input>"
+          MaybeT $ return Nothing
+      file <- liftIO $ mmapFileByteString inputFile Nothing
+      lib <- MaybeT $ openLibrary libName file
+      parser <- MaybeT $ getParser lib parserName
+      inp <- liftIO getContents
+      liftIO $ mapM_ (procLine parser) (lines inp)
