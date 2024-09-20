809c4f65a02b1e3a07188024ece4e12be00dbbea
8051enthusiast <8051enthusiast@protonmail.com>
2024-09-20 10:33:21 +0200

    ybq: add option to print binary

--- tools/ybq/app/Main.hs
+++ tools/ybq/app/Main.hs
@@ -1,18 +1,55 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use lambda-case" #-}
 module Main (main) where
 
 import Control.Monad.Trans (liftIO)
 import Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
-import Lib (YaboVal, getParser, openLibrary, parse)
+import Data.ByteString (ByteString, pack)
+import Data.ByteString qualified as ByteString
+import Data.Foldable (toList)
+import Data.Word (Word8)
+import GHC.IO.Handle.FD (stderr)
+import Lib (YaboVal (..), getParser, openLibrary, parse)
 import System.Environment (getArgs)
-import System.IO.MMap (mmapFileForeignPtr, Mode (ReadOnly))
+import System.IO (hPutStrLn)
+import System.IO.MMap (Mode (ReadOnly), mmapFileForeignPtr)
+import ValGen (ValGen (Cons, Error))
 
+toChar :: Integer -> Maybe Word8
+toChar x = if x > fromChr (maxBound :: Word8) || x < fromChr (minBound :: Word8) then Nothing else Just $ toChr x
+  where
+    fromChr = toInteger . fromEnum
+    toChr = toEnum . fromInteger
 
-procLine :: YaboVal -> String -> IO ()
-procLine parser line = do
+valToBinary :: YaboVal -> Maybe ByteString
+valToBinary (YaboArray s) =
+  pack . toList
+    <$> mapM
+      ( \x ->
+          case x of
+            YaboInt i -> toChar i
+            _ -> Nothing
+      )
+      s
+valToBinary _ = Nothing
+
+printBin :: ValGen YaboVal -> IO ()
+printBin (ValGen.Cons x xs) = case valToBinary x of
+  Just bin -> do
+    ByteString.putStr bin
+    printBin xs
+  Nothing -> do
+    hPutStrLn stderr "Could not print, not an array of bytes"
+printBin (ValGen.Error res) = putStr ("Error: " ++ res)
+printBin _ = pure ()
+
+procLine :: Bool -> YaboVal -> String -> IO ()
+procLine binary parser line = do
   parseResult <- parse line "stdin"
   case parseResult of
     Left err -> print err
-    Right program -> print $ program parser
+    Right program -> if not binary then print $ program parser else printBin $ program parser
 
 main :: IO ()
 main =
@@ -20,13 +57,14 @@ main =
   where
     prog = do
       args <- liftIO getArgs
-      (libName, parserName, inputFile) <- case args of
-        [library, parser, input] -> return (library, parser, input)
+      (binary, libName, parserName, inputFile) <- case args of
+        [library, parser, input] -> return (False, library, parser, input)
+        ["-b", library, parser, input] -> return (True, library, parser, input)
         _ -> do
-          liftIO $ putStrLn "Usage: ybq <library> <parser> <input>"
+          liftIO $ putStrLn "Usage: ybq [-b] <library> <parser> <input>"
           MaybeT $ return Nothing
       (file, _, size) <- liftIO $ mmapFileForeignPtr inputFile ReadOnly Nothing
       lib <- MaybeT $ openLibrary libName (file, size)
       parser <- MaybeT $ getParser lib parserName 0
       inp <- liftIO getContents
-      liftIO $ mapM_ (procLine parser) $ filter (/= "") (lines inp)
+      liftIO $ mapM_ (procLine binary parser) $ filter (/= "") (lines inp)
