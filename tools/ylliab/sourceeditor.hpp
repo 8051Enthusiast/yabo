c823b0a336bf04c9925283065c077a10e798fb54
8051enthusiast <8051enthusiast@protonmail.com>
2024-10-16 00:57:33 +0200

    ylliab: add line numbers to editor
    following https://doc.qt.io/qt-6.2/qtwidgets-widgets-codeeditor-example.html because ugh

--- tools/ylliab/sourceeditor.hpp
+++ tools/ylliab/sourceeditor.hpp
@@ -3,12 +3,38 @@
 #include <QObject>
 #include <QPlainTextEdit>
 #include <QRegularExpression>
+#include <QWidget>
+
+class SourceEditor;
+
+class LineNumberArea : public QWidget {
+public:
+  LineNumberArea(SourceEditor *editor);
+
+  QSize sizeHint() const override;
+
+protected:
+  void paintEvent(QPaintEvent *event) override;
+
+private:
+  SourceEditor *source_editor;
+};
 
 class SourceEditor : public QPlainTextEdit {
   Q_OBJECT
 public:
   SourceEditor(QWidget *parent = nullptr);
-  virtual void keyPressEvent(QKeyEvent *event) override;
+  void keyPressEvent(QKeyEvent *event) override;
+
+  int line_number_width();
+  void line_number_area_paint_event(QPaintEvent *event);
+
+protected:
+  void resizeEvent(QResizeEvent *event) override;
+
+private slots:
+  void update_line_number_area_width(int newBlockCount);
+  void update_line_number_area(const QRect &rect, int dy);
 
 private:
   struct CurrentLineInfo {
@@ -16,6 +42,8 @@ private:
     int indent;
   };
   CurrentLineInfo current_line_info() const;
+
+  LineNumberArea *line_number_area;
   static constexpr int tab_width = 2;
   static const QRegularExpression indent_re;
 };
