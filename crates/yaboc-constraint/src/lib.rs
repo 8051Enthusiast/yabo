51a7c4d4f5473cea3d7fe380fc16ffd18743519f
8051enthusiast <8051enthusiast@protonmail.com>
2024-10-18 06:32:43 +0200

    rename when -> if
    if is no longer used by the pattern matching

--- crates/yaboc-constraint/src/lib.rs
+++ crates/yaboc-constraint/src/lib.rs
@@ -474,12 +474,12 @@ mod tests {
     fn backtrack_dep() {
         let ctx = Context::<ConstraintTestDatabase>::mock(
             r#"
-            def ~backtrack_if(x: int) = x is 0 then [1] else [2]
-            def ~backtrack_when(x: int) = when?(x == 0) then [1] else [2]
+            def ~backtrack_is(x: int) = x is 0 then [1] else [2]
+            def ~backtrack_if(x: int) = if?(x == 0) then [1] else [2]
             def ~backtrack_field(x: int) = {| let y = x is 0 |}.?y then [1] else [2]
             "#,
         );
-        for parser_name in ["backtrack_if", "backtrack_when", "backtrack_field"] {
+        for parser_name in ["backtrack_is", "backtrack_if", "backtrack_field"] {
             let parser = ctx.parser(parser_name);
             let len = ctx.db.fun_len(parser);
             let Val::Static(1, deps) = len else {
