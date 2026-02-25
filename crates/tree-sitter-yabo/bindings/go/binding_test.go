3655bf139b6eb3772f0488ef2c547a43d08407ff
8051enthusiast <8051enthusiast@protonmail.com>
2026-02-25 22:38:52 +0100

    update tree-sitter

--- crates/tree-sitter-yabo/bindings/go/binding_test.go
+++ crates/tree-sitter-yabo/bindings/go/binding_test.go
@@ -3,13 +3,13 @@ package tree_sitter_yabo_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-yabo"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_yabo "github.com/8051enthusiast/yabo/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
 	language := tree_sitter.NewLanguage(tree_sitter_yabo.Language())
 	if language == nil {
-		t.Errorf("Error loading Yabo grammar")
+		t.Errorf("Error loading yabo grammar")
 	}
 }
---
+++ crates/tree-sitter-yabo/go.mod
@@ -0,0 +1,5 @@
+module github.com/8051enthusiast/yabo
+
+go 1.22
+
+require github.com/tree-sitter/go-tree-sitter v0.24.0
